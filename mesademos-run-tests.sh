1030bcde91156fec37336d60f3908eb94201a0ea
Dylan Baker <dylan@pnwbakers.com>
2017-03-25 10:45:22 -0700

    add debugging

--- mesademos-run-tests.sh
+++ mesademos-run-tests.sh
@@ -2,9 +2,9 @@
 
 if [[ -n "$TRAVIS" ]]; then
   git clone https://github.com/dcbaker/mesa-demos /root/mesa-demos
-  git checkout wip/meson
   cd /root/mesa-demos
 fi
 
+echo $PWD
 meson build "$@" || exit $?
 ninja -C build || exit $?
