d058a2699d82da98a841789f4c5621ed438579cb
Dylan Baker <dylan@pnwbakers.com>
2017-03-24 17:53:01 -0700

    initial commit

---
+++ mesademos-run-tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ -n "$TRAVIS" ]]; then
+  git clone https://github.com/dcbaker/mesa-demos /root/mesa-demos
+  cd /root/mesa-demos
+fi
+
+meson build "$@" || exit $?
+ninja -C build || exit $?
